a8172198e0274c1acfa638dd0ba701b05d099669
Gomit Saha <gomit.dev@gmail.com>
2025-04-13 21:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
     const role = users[username].role;
     if (role === "admin") {
       result.innerHTML = "👑 Welcome Admin!";
-      // You can also redirect: window.location.href = "admin.html";
+      window.location.href = "admin.html";
     } else {
       result.innerHTML = "👤 Welcome User!";
-      // Or redirect: window.location.href = "user.html";
+      window.location.href = "user.html";
     }
   } else {
     result.textContent = "❌ Invalid username or password.";
